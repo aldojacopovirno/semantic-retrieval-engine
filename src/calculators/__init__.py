cb8b3502b5c76144201d2b9deca9b7f554b36b0c
Aldo Jacopo Virno <aldojacopovirno.finance@gmail.com>
2025-01-11 17:52:01 +0100

    Create __init__.py

---
+++ src/calculators/__init__.py
@@ -0,0 +1,7 @@
+"""
+Similarity and relevance calculation utilities.
+"""
+from .similarity_calculator import SimilarityCalculator
+from .relevance_calculator import RelevanceCalculator
+
+__all__ = ['SimilarityCalculator', 'RelevanceCalculator']
